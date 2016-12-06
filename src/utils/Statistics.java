be027857bc35ab7a96e8ab9af8ceec75f8b70df8
ksemer <ksemer@cs.uoi.gr>
2016-12-06 18:21:55 +0200

    Stable version Adaptive ranking, Loader fix, etc
    1) Rename halfway to adaptive ranking
    2) Finish adaptive ranking implementation
    3) Major fixes in loader classes
    4) More renames
    5) etc

---
+++ src/utils/Statistics.java
@@ -0,0 +1,53 @@
+package utils;
+
+import graph.version.Graph;
+import graph.version.Node;
+import graph.version.loader.LoaderDBLP;
+import graph.version.loader.LoaderProteins;
+import graph.version.loader.LoaderYT;
+import system.Config;
+
+public class Statistics {
+	public static String dataset = "";
+
+	public static void main(String[] args) throws Exception {
+		
+		Config.loadConfigs();
+		
+		Config.STORE_OBJECT = false;
+		Config.LOAD_OBJECT = false;
+		Config.TINLA_ENABLED = false;
+		Config.TIPLA_ENABLED = false;
+		Config.CTINLA_ENABLED = false;
+		
+		Config.PATH_DATASET = "/home/ksemer/workspaces/tkde_data/protein/pcms/PCMS.gfu";
+
+		String dataset = Config.PATH_DATASET.toLowerCase();
+		Graph lvg;
+
+		// for dblp dataset
+		if (dataset.contains("dblp")) {
+			lvg = new LoaderDBLP().loadDataset();
+		}
+		// for yt dataset
+		else if (dataset.contains("yt"))
+			// TODO number of changes for yt
+			lvg = new LoaderYT(9).loadDataset();
+		// for proteins
+		else
+			lvg = new LoaderProteins().loadDataset();
+		
+		
+		System.out.println("Nodes: " + lvg.size());
+		int edges = 0;
+		
+		for (Node n : lvg.getNodes()) {
+			edges+= n.getAdjacency().size();
+		}
+		
+		if (!Config.ISDIRECTED)
+			edges/= 2;
+		
+		System.out.println("Edges: " + edges);
+	}
+}
--- src/utils/dblp/LabelPublicationsStats.java
+++ src/utils/dblp/LabelPublicationsStats.java
@@ -11,19 +11,22 @@ import java.io.IOException;
  *
  */
 public class LabelPublicationsStats {
-	private static final int N = 0;
+	private static String dataset = "/home/ksemer/workspaces/tkde_data/dblp_all_dataset/dblp_publications_labels";
+	private static int MAX_INTERVAL = 58;
+
 	private static final int BEGINNER = 2;
 	private static final int JUNIOR = 5;
 	private static final int SENIOR = 10;
 	private static final int PROF = 11;
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("files/DBLP_Authors_Attr"));
+		BufferedReader br = new BufferedReader(new FileReader(dataset));
 		String line = null;
 		String[] id, publicationsPerYear;
 
-		int[][] ranking = new int[57][5];
+		int[][] ranking = new int[MAX_INTERVAL][4];
 
+		br.readLine();
 		while ((line = br.readLine()) != null) {
 			id = line.split("\\|");
 			publicationsPerYear = id[1].split(",");
@@ -32,21 +35,21 @@ public class LabelPublicationsStats {
 			for (int i = 0; i < publicationsPerYear.length; i++) {
 				number = Integer.parseInt(publicationsPerYear[i]);
 
-				if (number == N)
+				if (number <= BEGINNER)
 					ranking[i][0]++;
-				else if (number <= BEGINNER)
-					ranking[i][1]++;
 				else if (number <= JUNIOR)
-					ranking[i][2]++;
+					ranking[i][1]++;
 				else if (number <= SENIOR)
-					ranking[i][3]++;
+					ranking[i][2]++;
 				else if (number >= PROF)
-					ranking[i][4]++;
+					ranking[i][3]++;
 			}
 		}
 		br.close();
 
 		for (int i = 0; i < ranking.length; i++) {
+			System.out.print((1959 + i) + "\t");
+
 			for (int j = 0; j < ranking[0].length; j++) {
 				System.out.print(ranking[i][j] + "\t");
 			}
